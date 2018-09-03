82d72194aa58d6c7e2b628a3b7c5c7bffd4a8d84
Tobias Knopp <tobias@knoppweb.de>
2018-09-03 20:02:46 +0000

    fix SystemMatrix.jl

--- src/SystemMatrix.jl
+++ src/SystemMatrix.jl
@@ -15,7 +15,7 @@ function converttoreal{T}(S::AbstractArray{Complex{T},2})
 end
 
 function getSystemMatrix(f::MPIFile,
-           frequencies=1:rxNumFrequencies(f)*rxNumChannels(f)*acqNumPeriodsPerFrame(f);
+           frequencies=1:rxNumFrequencies(f)*rxNumChannels(f);
                          bgCorrection=false, loadasreal=false,
                          kargs...)
   #if measIsTransposed(f) && measIsFourierTransformed(f)
